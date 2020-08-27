1fc1d0154a4fd24437bddb1b0a124cd40911181a
Tony <363806348@qq.com>
2020-08-27 15:21:03 +0800

    initialize DQN

---
+++ DQN/deep_q_learning_with_experience_replay.py
@@ -0,0 +1,17 @@
+import gym
+import DQN.Network as Network
+
+
+class Agent:
+    def __init__(self, k_frames):
+        # TODO
+        return
+
+    def running(self):
+        return
+
+
+if __name__ == '__main__':
+    env = gym.make('Breakout-v0')
+
+
