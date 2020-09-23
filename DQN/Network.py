f5da2fd4864bd8e90aef5d19f1a131081b84a8cf
tony <363806348@qq.com>
2020-09-23 18:40:20 +0800

    debug DQN with experience

--- DQN/Network.py
+++ DQN/Network.py
@@ -5,9 +5,10 @@ import torch.nn.functional as F
 class Net(nn.Module):
     def __init__(self, input_size, output_size):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(input_size, 16, kernel_size=8, stride=4)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=4, stride=2)
-        self.fc = nn.Linear(32 * 9 * 9, 512)
+        self.conv1 = nn.Conv2d(input_size, 32, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+        self.fc = nn.Linear(4096, 512)
         self.fc_2 = nn.Linear(512, output_size)
 
     def forward(self, x):
@@ -15,7 +16,9 @@ class Net(nn.Module):
         x = F.relu(x)
         x = self.conv2(x)
         x = F.relu(x)
-        x = x.view(-1, 32 * 9 * 9)
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = x.view(-1, 4096)
         x = self.fc(x)
         x = F.relu(x)
         x = self.fc_2(x)
--- DQN/dqn_with_experience.py
+++ DQN/dqn_with_experience.py
@@ -8,6 +8,7 @@ import torch.optim as optim
 import numpy as np
 import torch.nn as nn
 from torch.utils.tensorboard import SummaryWriter
+from collections import deque
 import random
 import time
 import os
@@ -15,28 +16,24 @@ import os
 
 class CustomDataset(utils_data.Dataset):
     def __init__(self, data, labels):
-        self.data = data
+        self.data = np.array(data)
         self.labels = labels
 
     def __getitem__(self, item):
-        state_4_channels = np.array([self.data[item], self.data[item + 1],
-                                     self.data[item + 2], self.data[item + 3]]).astype(np.float32)
-        return state_4_channels, self.labels[item]
+        return self.data[item], self.labels[item]
 
     def __len__(self):
-        return len(self.data) - 3
+        return len(self.data)
 
 
 class Agent:
-    def __init__(self, env, k_frames=4, phi_temp_size=4, model_path='./model/'):
+    def __init__(self, env, k_frames=1, phi_temp_size=4, model_path='./model/'):
         self.env = env
         self.env_action_n = self.env.action_space.n
         self.state_value_function = Network.Net(4, self.env_action_n)
         self.k_frames = k_frames
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.state_value_function.to(self.device)
-        self.criterion = nn.SmoothL1Loss() # nn.MSELoss()
-        self.optimizer = optim.SGD(self.state_value_function.parameters(), lr=0.001, momentum=0.9)
         self.writer = SummaryWriter("./log/")
         self.down_sample_size = 84
         self.model_path = model_path
@@ -52,8 +49,8 @@ class Agent:
     def convert_down_sample(self, state):
         image = np.array(state)
         gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        gray_img = cv2.resize(gray_img, (self.down_sample_size, 110))
-        return gray_img[110 - self.down_sample_size:110, 0:self.down_sample_size] / 255. - 0.5
+        gray_img = cv2.resize(gray_img, (92, 110))
+        return gray_img[110 - 92:110, 0:92] / 255 - 0.5
 
     def select_action(self, state_phi, epsilon):
         policies = np.zeros(self.env.action_space.n)
@@ -64,7 +61,7 @@ class Agent:
             np.flatnonzero(value_of_action_list == value_of_action_list.max()))
         for action_iter in range(self.env.action_space.n):
             if action_iter == optimal_action:
-                policies[action_iter] = 1 - epsilon + epsilon / self.env.action_space.n
+                policies[action_iter] = 1. - epsilon + epsilon / self.env.action_space.n
             else:
                 policies[action_iter] = epsilon / self.env.action_space.n
         probability_distribution = policies
@@ -82,127 +79,140 @@ class Agent:
         is_done = 0
         others = 0
         for i in range(self.k_frames):
-            new_state, reward, is_done, others = self.env.step(action)
+            new_state, r, is_done, others = self.env.step(action)
+            reward += r
             if is_done:
                 break
         return new_state, reward, is_done, others
 
-    def train_state_value_function(self, epoch_num, memory, bach_size, gamma=1.0):
+    def train_state_value_function(self, memory, bach_size, gamma=0.999):
 
+        # training parameter
+
+        criterion = nn.SmoothL1Loss()
+        optimizer = optim.Adam(self.state_value_function.parameters(), lr=1e-6)
         total_loss = 0
-        for epo_i in range(epoch_num):
-            # build label:
-            is_done_array = [mem[4] for mem in memory]
-            next_state_data = [mem[3] for mem in memory]
-            reward_array = [mem[2] for mem in memory]
-            action_array = [mem[1] for mem in memory]
-            state_data = [mem[0] for mem in memory]
+        # build label:
+        next_state_data = [mem[3] for mem in memory]
+        reward_array = [mem[2] for mem in memory]
+        action_array = [[int(mem[1])] for mem in memory]
+        state_data = [mem[0] for mem in memory]
+        next_state_max_value = []
+        fake_labels = np.zeros(len(next_state_data))
+        next_state_data_set = CustomDataset(next_state_data, fake_labels)
+        next_state_data_loader = DataLoader(next_state_data_set, batch_size=bach_size, shuffle=False)
 
-            next_state_max_value = []
-            fake_labels = np.zeros(len(next_state_data))
-            next_state_data_set = CustomDataset(next_state_data, fake_labels)
-            next_state_data_loader = DataLoader(next_state_data_set, batch_size=bach_size, shuffle=False)
-
-            with torch.no_grad():
-                for i, data in enumerate(next_state_data_loader, 0):
-                    # get the inputs; data is a list of [inputs, labels]
-                    inputs, labels = data
-                    inputs = inputs.to(self.device)
-                    outputs = self.state_value_function(inputs)
-                    _, predictions = torch.max(outputs, 1)
-                    outputs = outputs.cpu().numpy()
-                    predictions = predictions.cpu().numpy()
-                    for p_i in range(len(predictions)):
-                        next_state_max_value.append(outputs[p_i][predictions[p_i]])
-
-            for is_done_i in range(len(is_done_array) - 3):
-                if is_done_array[is_done_i + 3]:
-                    next_state_max_value[is_done_i] = 0
-            reward_array = np.array(reward_array)[3:] + gamma * np.array(next_state_max_value)
-
-            labels_array = []
-            reward_array = reward_array.astype(np.float32)
-            for v_i in range(len(reward_array)):
-                label = np.zeros(self.env_action_n)
-                label[action_array[v_i + 3]] = reward_array[v_i]
-                labels_array.append(label.astype(np.float32))
-
-            # train the model
-            data_set = CustomDataset(state_data, labels_array)
-            data_loader = DataLoader(data_set, batch_size=bach_size, shuffle=True)
-            for i, data in enumerate(data_loader, 0):
+        with torch.no_grad():
+            for i, data in enumerate(next_state_data_loader, 0):
                 # get the inputs; data is a list of [inputs, labels]
                 inputs, labels = data
                 inputs = inputs.to(self.device)
-                labels = labels.to(self.device)
-                # zero the parameter gradients
-                self.optimizer.zero_grad()
-                # forward + backward + optimize
                 outputs = self.state_value_function(inputs)
-                for label_i in range(len(outputs)):
-                    for output_i in range(len(outputs[label_i])):
-                        if labels[label_i][output_i] == 0:
-                            outputs[label_i][output_i] = 0
-                loss = self.criterion(outputs, labels)
-                loss.backward()
+                _, predictions = torch.max(outputs, 1)
+                outputs = outputs.cpu().numpy()
+                predictions = predictions.cpu().numpy()
+                for p_i in range(len(predictions)):
+                    next_state_max_value.append(outputs[p_i][predictions[p_i]])
+                # if random.randint(0, 10000) < 10:
+                #     print(predictions)
+                #     print(next_state_max_value)
+        for is_done_i in range(len(memory)):
+            if memory[is_done_i][4]:
+                next_state_max_value[is_done_i] = 0
+        reward_array = np.array(reward_array) + gamma * np.array(next_state_max_value)
+        reward_array = reward_array.transpose().astype(np.float32)
+        action_array = torch.Tensor(action_array).long()
+        # labels_array = []
+        # reward_array = reward_array.astype(np.float32)
+        # for v_i in range(len(reward_array)):
+        #     label = np.zeros(self.env_action_n)
+        #     label[action_array[v_i]] = reward_array[v_i]
+        #     labels_array.append(label.astype(np.float32))
 
-                self.optimizer.step()
-                total_loss += loss.item()
+        # train the model
+        # data_set = CustomDataset(state_data, labels_array)
+        data_set = CustomDataset(state_data, reward_array)
+        data_loader = DataLoader(data_set, batch_size=bach_size, shuffle=False)
+        for i, data in enumerate(data_loader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+            inputs = inputs.to(self.device)
+            labels = labels.to(self.device).view(-1, 1)
+            actions = action_array.to(self.device)
+            # zero the parameter gradients
+            optimizer.zero_grad()
+            # forward + backward + optimize
+            outputs = self.state_value_function(inputs).gather(0, actions)
 
-                if i % 100 == 0 and i != 0:
-                    # record
-                    iteration = (epo_i - 1) * len(data_loader) + i
-                    self.writer.add_scalar('train/loss', total_loss / 100., iteration)
-                    now = int(round(time.time() * 1000))
-                    now02 = time.strftime('%H-%M-%S', time.localtime(now / 1000))
-                    print(now02 + ' [%d, %d] loss: %.7f' %
-                          (epo_i + 1, i + 1, total_loss / 100.))
-                    total_loss = 0.0
+            loss = criterion(outputs, labels)
+            loss.backward()
 
-    def running(self, episodes_num, mini_bach_size, memory_length=30000):
-        memory = []
+            optimizer.step()
+            total_loss += loss.item()
+            # record
+            self.writer.add_scalar('train/loss', total_loss)
+            # now = int(round(time.time() * 1000))
+            # now02 = time.strftime('%H-%M-%S', time.localtime(now / 1000))
+            # print(now02 + ' [%d, %d] loss: %.7f' %
+            #       (epo_i + 1, i + 1, total_loss / 100.))
+            # total_loss = 0.0
+
+    def running(self, episodes_num, mini_bach_size,epsilon_start=0.9,
+                epsilon_end=0.1, epsilon_update=100, memory_length=20000):
+        memory = deque(maxlen=memory_length)
         frame_num = 0
+        epsilon = epsilon_start
         for episode_i in range(1, episodes_num):
-            epsilon = 0.4
+            # set a dynamic epsilon
+            if episode_i % epsilon_update == 0 and epsilon > epsilon_end:
+                epsilon -= 0.001
             total_reward = 0
+            # random choice an action at the beginning of the process
             action = np.random.choice(self.env_action_n, 1)[0]
             state = self.env.reset()
             state = self.convert_down_sample(np.array(state))
+            # add state into a list to create a phi
+            # another three states are needed
             self.phi_temp.append(state)
             for i in range(1, 4):
                 new_state, reward, is_done, _ = self.skip_k_frame(action)
                 frame_num += 1
                 new_state = self.convert_down_sample(np.array(new_state))
                 self.phi_temp.append(new_state)
-                memory.append([state, action, reward, new_state, is_done])
-                state = new_state
+            # create phi
             state_phi = self.phi()
+            # select action according the first phi
             action = self.select_action(state_phi, epsilon)
             new_state, reward, is_done, _ = self.skip_k_frame(action)
             frame_num += 1
             total_reward += reward
+            # down sample and add the new state into the list to create phi
             new_state = self.convert_down_sample(np.array(new_state))
             self.phi_temp.append(new_state)
-            memory.append([state, action, reward, new_state, is_done])
-            state = new_state
+            new_state_phi = self.phi()
+            # add phi(state) and phi(new state) and reward and action into memory
+            memory.append([state_phi, action, reward, new_state_phi, is_done])
+
             while not is_done:
-                state_phi = self.phi()
+                state_phi = new_state_phi
                 action = self.select_action(state_phi, epsilon)
                 new_state, reward, is_done, _ = self.skip_k_frame(action)
+
                 frame_num += 1
                 total_reward += reward
                 new_state = self.convert_down_sample(np.array(new_state))
-                memory.append([state, action, reward, new_state, is_done])
+                cv2.imshow('new_state', np.array((new_state + 0.5) * 255.).astype(np.uint8))
+                cv2.waitKey(1)
                 self.phi_temp.append(new_state)
-                state = new_state
-                # skip the next k frames by executing the last action
-            if len(memory) > memory_length:
-                self.train_state_value_function(epoch_num=1, memory=memory, bach_size=mini_bach_size)
-                memory = memory[len(memory) - memory_length:len(memory)]
+                new_state_phi = self.phi()
+                memory.append([state_phi, action, reward, new_state_phi, is_done])
+                if len(memory) >= memory_length:
+                    sub_memory = random.sample(memory, mini_bach_size)
+                    self.train_state_value_function(memory=sub_memory, bach_size=mini_bach_size)
             print("reward of episode: " + str(episode_i) + " is " + str(total_reward)
                   + " and frame number is " + str(frame_num))
             self.writer.add_scalar('reward of episode', total_reward, episode_i)
-            if episode_i % 500 == 0:
+            if episode_i % 100 == 0:
                 print('model saved')
                 now = int(round(time.time() * 1000))
                 now02 = time.strftime('%m-%d-%H-%M-%S', time.localtime(now / 1000))
@@ -216,4 +226,4 @@ class Agent:
 if __name__ == '__main__':
     env = gym.make('Pong-v0')
     agent = Agent(env)
-    agent.running(episodes_num=100000, mini_bach_size=32)
+    agent.running(episodes_num=10000, mini_bach_size=32)
